5e52daccf6cd3f86d22ef55b283b16aa1fabdf27
Soundradevi-S-R <soundradevi.rajagopal@gmail.com>
2024-09-09 22:47:02 +0530

    countries-search-complete

---
+++ countries-search/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import CountriesSearch from "./CountriesSearch.jsx";
+
+function App() {
+  return (
+    <div className="App">
+     <CountriesSearch/>
+    </div>
+  );
+}
+
+export default App;
---
+++ countries-search/src/CountriesSearch.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import styles from  "./CountriesSearch.module.css";
+import { useEffect,useState } from "react";
+
+
+const CountryCard =({flagImg,name}) =>{
+
+   //console.log("print ", data);
+    return (
+        <div className={styles.carddiv} >
+            
+            <img src={flagImg} alt={name} 
+                                style={{
+                                    width:"100px",
+                                    height:"100px",
+                                    }}/>
+            <h2>{name}</h2>
+       </div>);
+};
+
+
+function CountriesSearch(){
+  
+    const API_URL="https://restcountries.com/v3.1/all";
+    
+    const [countriesData,setCountriesData] = useState([]);
+    const [filterData,setFilterData] = useState([]);
+    const [searchVal, setSearchVal] = useState('');
+
+
+
+    
+    async function apiCall() {
+        try{
+          const response = await fetch(API_URL);
+          const data = await response.json();            
+            setCountriesData(data);
+            setFilterData(data);     
+           
+         }catch(error){
+            console.error("Error fetching data:",error);
+        }              
+    }
+
+    useEffect(()=>{
+        apiCall();      
+                  
+    },[]);
+
+    const filterSearch=(e)=>{
+        setSearchVal(e.target.value);
+        let filtered = countriesData.filter((country) => 
+                                country.name.common.toLowerCase().includes(searchVal.toLocaleLowerCase()) );
+        setFilterData(filtered);    
+
+    };
+
+    return(
+    <div>
+        <div className={styles.searchDiv}>
+            <input 
+                 type='text'
+                 placeholder="Search for Countries" 
+                 className={styles.search}
+                 value={searchVal}
+                 onChange={filterSearch}  />
+        </div>   
+        <div className={styles.gridcontainer}>
+            {
+                (filterData.length && 
+                filterData.map((country) =>  <CountryCard key={country.name.common} flagImg={country.flags.png} name={country.name.common} />
+                 ))
+            }   
+
+        </div>
+    </div>
+    );
+
+}
+export default CountriesSearch;
